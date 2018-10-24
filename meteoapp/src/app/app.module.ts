b7d1dd9d4b72a9465823012267d59f902a3f7ca3
Deo Kamara <deokamara@Deos-MacBook-Pro.local>
2018-10-24 19:36:13 +0300

    [Feature 61438853] Add search functionality

--- meteoapp/src/app/app.module.ts
+++ meteoapp/src/app/app.module.ts
@@ -11,6 +11,7 @@ import {MatButtonModule} from '@angular/material/button';
 import {MatFormFieldModule,} from '@angular/material/form-field';
 import { RouterModule } from '@angular/router';
 import { SignupComponent } from './signup/signup.component';
+import { GetWeatherService } from './services/search/search.service';
 import { LoginComponent } from './login/login.component';
 import {HttpClientModule} from '@angular/common/http';
 import { SignupService } from './services/signup/signup.service';
@@ -21,7 +22,9 @@ import {
   MatNativeDateModule
 } from '@angular/material';
 import {MatDatepickerModule} from '@angular/material/datepicker';
-import { MatMomentDateModule } from "@angular/material-moment-adapter";
+import { MatMomentDateModule, MAT_MOMENT_DATE_FORMATS } from "@angular/material-moment-adapter";
+import { CookieService } from 'ngx-cookie-service';
+import {DateAdapter, MAT_DATE_FORMATS, MAT_DATE_LOCALE} from '@angular/material/core';
 @NgModule({
   declarations: [
     AppComponent,
@@ -53,9 +56,11 @@ import { MatMomentDateModule } from "@angular/material-moment-adapter";
     ])
   ],
   providers: [SignupComponent,
+    {provide: MAT_DATE_LOCALE, useValue: 'en-GB'},
+    CookieService,
     LoginComponent,
     SignupService,
-    LoginService],
+    LoginService,GetWeatherService],
   bootstrap: [AppComponent,
   
   ]
