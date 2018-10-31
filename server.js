405473d87ffbb67464470d6d33e43c392a453709
Deo Kamara <deokamara@Deos-MacBook-Pro.local>
2018-10-31 18:35:05 +0300

    Host on heroku

--- server.js
+++ server.js
@@ -1,17 +1,14 @@
-//Install express server
 const express = require('express');
 const path = require('path');
-const cors = require('cors')
+
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/meteoApp'));
+app.use(express.static(__dirname + '/dist/meteoapp'));
 
-// app.get('/*', function(req,res) {
+app.get('/*', function(req,res) {
     
-// res.sendFile(path.join(__dirname+'/dist/meteoapp/index.html'));
-// });
-app.use(cors());
-app.use(express.static('./meteoapp/dist/meteoapp'));
-// Start the app by listening on the default Heroku port
+res.sendFile(path.join(__dirname+'/dist/meteoapp/index.html'));
+});
 app.listen(process.env.PORT || 4200);
+// Start the app by listening on the default Heroku port
