1d532e898ac4246b89d287d298540f99b11aa34f
Deo Kamara <deokamara@Deos-MacBook-Pro.local>
2018-11-04 19:49:43 +0300

    Include date

--- src/app/services/savedata/savedata.service.ts
+++ src/app/services/savedata/savedata.service.ts
@@ -1,12 +1,13 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import {HttpHeaders} from '@angular/common/http'
+import {HttpHeaders} from '@angular/common/http';
+import {MatSnackBar} from '@angular/material';
 @Injectable({
   providedIn: 'root'
 })
 export class SavedataService {
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, public snackBar: MatSnackBar) { }
   savedata(data, token){
 
     const uri = 'https://meteoapiexpress.herokuapp.com/ul/data';
@@ -17,6 +18,10 @@ export class SavedataService {
       'x-access-token': token,
     })
   };
-   return this.http.post(uri, obj,httpOptions).subscribe(res => console.log(res));
+   return this.http.post(uri, obj,httpOptions).subscribe((res) => {
+    this.snackBar.open(res['Message'],'',{
+      duration: 500,
+    });
+     console.log(res)});
   }
 }
