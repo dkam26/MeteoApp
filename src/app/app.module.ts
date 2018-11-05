7598ab2a5a87e0f338b98b4e44db373500f77f20
Deo Kamara <deokamara@Deos-MacBook-Pro.local>
2018-11-05 08:00:31 +0300

    Clear console.log errors

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -32,6 +32,7 @@ import { NavbarComponent } from './navbar/navbar.component';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import { DisplaydataComponent } from './displaydata/displaydata.component';
 
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -41,6 +42,7 @@ import { DisplaydataComponent } from './displaydata/displaydata.component';
     SearchresultsComponent,
     NavbarComponent,
     DisplaydataComponent,
+   
   
   ],
   imports: [BrowserAnimationsModule,
@@ -60,6 +62,7 @@ import { DisplaydataComponent } from './displaydata/displaydata.component';
     HttpClientModule,
     FormsModule,
     MatDatepickerModule,
+  
     RouterModule.forChild([
       { path: 'signup', component:SignupComponent},
       {path:'login', component:LoginComponent},
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { LoginService } from '../services/login/login.service';
-
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
