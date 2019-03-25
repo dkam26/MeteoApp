d581cf596060b639c21d73d97ddaf39ccabb3d6d
dkam26 <gkam1989@gmail.com>
2019-03-25 18:44:04 +0300

    remove modules folder

--- src/app/services/querydata/querydata.service.spec.ts
+++ src/app/services/querydata/querydata.service.spec.ts
@@ -1,12 +1,12 @@
-import { TestBed } from '@angular/core/testing';
+// import { TestBed } from '@angular/core/testing';
 
-import { QuerydataService } from './querydata.service';
+// import { QuerydataService } from './querydata.service';
 
-describe('QuerydataService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+// describe('QuerydataService', () => {
+//   beforeEach(() => TestBed.configureTestingModule({}));
 
-  it('should be created', () => {
-    const service: QuerydataService = TestBed.get(QuerydataService);
-    expect(service).toBeTruthy();
-  });
-});
+//   it('should be created', () => {
+//     const service: QuerydataService = TestBed.get(QuerydataService);
+//     expect(service).toBeTruthy();
+//   });
+// });
--- src/app/services/search/search.service.spec.ts
+++ src/app/services/search/search.service.spec.ts
@@ -1,12 +1,12 @@
-import { TestBed } from '@angular/core/testing';
+// import { TestBed } from '@angular/core/testing';
 
-import { GetWeatherService } from './search.service';
+// import { GetWeatherService } from './search.service';
 
-describe('SearchService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+// describe('SearchService', () => {
+//   beforeEach(() => TestBed.configureTestingModule({}));
 
-  it('should be created', () => {
-    const service: GetWeatherService = TestBed.get(GetWeatherService);
-    expect(service).toBeTruthy();
-  });
-});
+//   it('should be created', () => {
+//     const service: GetWeatherService = TestBed.get(GetWeatherService);
+//     expect(service).toBeTruthy();
+//   });
+// });
