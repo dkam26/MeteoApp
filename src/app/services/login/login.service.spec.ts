a6442334bbff32374fa768b321c7b6a1bb766b6f
dkam26 <gkam1989@gmail.com>
2019-04-03 20:09:26 +0300

    test services

--- src/app/services/login/login.service.spec.ts
+++ src/app/services/login/login.service.spec.ts
@@ -1,12 +1,37 @@
-// import { TestBed } from '@angular/core/testing';
+import { TestBed } from '@angular/core/testing';
 
-// import { LoginService } from './login.service';
+import { LoginService } from './login.service';
+import {  HttpClientModule } from '@angular/common/http';
+import { RouterTestingModule } from '@angular/router/testing';
+import { CookieService } from 'ngx-cookie-service';
+import { RouterModule } from '@angular/router';
+import { MatInputModule, MatNativeDateModule, MatDatepickerModule, MatFormFieldModule, MatButtonModule, MatSnackBarModule, MatCardModule, MatIconModule, MatListModule, MatToolbarModule } from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+describe('LoginService', () => {
+  beforeEach(() => TestBed.configureTestingModule({
+    providers: [ RouterModule, CookieService],
+      imports: [
+        BrowserAnimationsModule,
+        MatInputModule,
+        MatNativeDateModule,
+        MatDatepickerModule,
+        MatFormFieldModule,
+        MatDatepickerModule,
+        RouterTestingModule,
+        MatButtonModule,
+        MatSnackBarModule,
+        FormsModule,
+        MatCardModule,
+        MatIconModule,
+        MatListModule,
+        HttpClientModule,
+        MatToolbarModule,
+  ]
+  }));
 
-// describe('LoginService', () => {
-//   beforeEach(() => TestBed.configureTestingModule({}));
-
-//   it('should be created', () => {
-//     const service: LoginService = TestBed.get(LoginService);
-//     expect(service).toBeTruthy();
-//   });
-// });
+  it('should be created', () => {
+    const service: LoginService = TestBed.get(LoginService);
+    expect(service).toBeTruthy();
+  });
+});
--- src/app/services/search/search.service.spec.ts
+++ src/app/services/search/search.service.spec.ts
@@ -1,12 +1,44 @@
-// import { TestBed } from '@angular/core/testing';
+import { TestBed } from '@angular/core/testing';
 
-// import { GetWeatherService } from './search.service';
+import { GetWeatherService } from './search.service';
+import { RouterModule } from '@angular/router';
+import { CookieService } from 'ngx-cookie-service';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatInputModule, MatNativeDateModule, MatDatepickerModule, MatFormFieldModule, MatButtonModule, MatSnackBarModule, MatCardModule, MatIconModule, MatListModule, MatToolbarModule } from '@angular/material';
+import { RouterTestingModule } from '@angular/router/testing';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { DisplaydataComponent } from 'src/app/displaydata/displaydata.component';
 
-// describe('SearchService', () => {
-//   beforeEach(() => TestBed.configureTestingModule({}));
+describe('SearchService', () => {
+  beforeEach(() => TestBed.configureTestingModule({
+    providers: [ RouterModule, CookieService],
+    declarations: [ DisplaydataComponent],
+    imports: [
+      BrowserAnimationsModule,
+      MatInputModule,
+      MatNativeDateModule,
+      MatDatepickerModule,
+      MatFormFieldModule,
+      MatDatepickerModule,
+      RouterTestingModule,
+      MatButtonModule,
+      MatSnackBarModule,
+      FormsModule,
+      MatCardModule,
+      MatIconModule,
+      MatListModule,
+      HttpClientModule,
+      MatToolbarModule,
+      RouterModule.forChild([
+          {path: 'data', component:DisplaydataComponent},
 
-//   it('should be created', () => {
-//     const service: GetWeatherService = TestBed.get(GetWeatherService);
-//     expect(service).toBeTruthy();
-//   });
-// });
+        ]),
+  ]
+  }));
+
+  it('should be created', () => {
+    const service: GetWeatherService = TestBed.get(GetWeatherService);
+    expect(service).toBeTruthy();
+  });
+});
